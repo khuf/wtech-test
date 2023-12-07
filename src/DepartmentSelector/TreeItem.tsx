631026d1451059f73464b062857a7e2132219ee1
Knut T. Hufthammer <knut.hufthammer@gmail.com>
2023-12-07 11:25:05 +0100

    fixed console errors

--- src/DepartmentSelector/TreeItem.tsx
+++ src/DepartmentSelector/TreeItem.tsx
@@ -26,8 +26,8 @@ const BranchNode = ({ node, toggleNode }: TreeNodeProps) => {
                     {collapsed ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                     </svg>
-                        : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+                        : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
                         </svg>}
                 </button>
                 <span className="border-red-800 border-l-4 pl-1.5">{`${node.data.title} (${selectedLeafNodeCount}/${leafNodeCount})`}</span>
